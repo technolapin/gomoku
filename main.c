52405f13abc0ce8cde7389f3f6e99df7ad15cc2d
technolapin <technolapin@orange.fr>
2019-01-21 11:58:46 +0100

    premier commit ^^

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-#define N 8
-#define BUT 5
+#define N 4
+#define BUT 3
 
 typedef enum joueur
   {NOIR, BLANC, VIDE}
@@ -33,26 +33,116 @@ a_gagne(Joueur* actuel,
 }
 
 Joueur
-gagne(Tableau tab[])
+gagne(Tableau tab)
 {
+  //horizontale
   for (int i = 0; i<N; i++)
     {
-      Joueur actuel = *tab[i][0];
+      Joueur actuel = VIDE;
       int compteur = 1;
       for (int j = 0; j<N; j++)
 	{
-	  Joueur gagnant = a_gagne(&actuel, &compteur, *tab[i][j]) != VIDE;
+	  Joueur gagnant = a_gagne(&actuel, &compteur, tab[i][j]);
 	  if (gagnant != VIDE)
 	    return gagnant;
 	}
       
     }
 
+  //verticale
+    for (int j = 0; j<N; j++)
+    {
+      Joueur actuel = VIDE;
+      int compteur = 1;
+      for (int i = 0; i<N; i++)
+	{
+	  Joueur gagnant = a_gagne(&actuel, &compteur, tab[i][j]);
+	  if (gagnant != VIDE)
+	    return gagnant;
+	}
+    }
+    //diagonale 1
+    for (int i = 0; i < N-BUT+1; i++)
+      {
+	Joueur actuel = VIDE;
+	int compteur = 1;
+
+	int j = 0;
+	for (int k = 0; k < N-i; k++)
+	  {
+	    Joueur gagnant = a_gagne(&actuel, &compteur, tab[i+k][j+k]);
+	    if (gagnant != VIDE)
+	      return gagnant;
+	    
+	  }
+      }
+
+    for (int i = 1; i < N-BUT+1; i++)
+      {
+	Joueur actuel = VIDE;
+	int compteur = 1;
+
+	int j = 0;
+	for (int k = 0; k < N-i; k++)
+	  {
+	    Joueur gagnant = a_gagne(&actuel, &compteur, tab[j+k][i+k]);
+	    if (gagnant != VIDE)
+	      return gagnant;
+	    
+	  }
+      }
+
+
+    //diagonale 2
+
+    for (int i = BUT-1; i < N; i++)
+      {
+	Joueur actuel = VIDE;
+	int compteur = 1;
+
+	int j = 0;
+	for (int k = 0; k <= i; k++)
+	  {
+	    Joueur gagnant = a_gagne(&actuel, &compteur, tab[i-k][j+k]);
+	    if (gagnant != VIDE)
+	      return gagnant;
+	    
+	  }
+      }
+
+    for (int i = 1; i < N-BUT+1; i++)
+      {
+	Joueur actuel = VIDE;
+	int compteur = 1;
+
+	int j = N-1;
+	for (int k = 0; k <= N-i+1; k++)
+	  {
+	    Joueur gagnant = a_gagne(&actuel, &compteur, tab[j-k][i+k]);
+	    if (gagnant != VIDE)
+	      return gagnant;
+	    
+	  }
+      }
+
+    
   return VIDE;
 }
 
 int
 main(void)
 {
+
+  Tableau tab = {
+		 {VIDE, VIDE, VIDE, NOIR},
+		 {VIDE, VIDE, VIDE, NOIR},
+		 {VIDE, VIDE, NOIR, VIDE},
+		 {VIDE, NOIR, VIDE, VIDE}
+  };
+  Joueur gagnant = gagne(tab);
+
+  printf("%d %d %d\n", gagnant==NOIR, gagnant==BLANC, gagnant==VIDE);
+
+  printf("%d, %d, %d\n", NOIR, BLANC, VIDE);
   return 0;
 }
